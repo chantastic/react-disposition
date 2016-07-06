74589abc7c3c750047cf07471c1b859c1e0b8c18
Michael Chan <mijoch@gmail.com>
2016-07-05 23:16:26 -0700

    config: add niave /lib build

---
+++ src/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // display
+  Inline: require("./Inline"),
+  InlineBlock: require("./InlineBlock"),
+  Block: require("./Block"),
+  Flex: require("./Flex"),
+  InlineFlex: require("./InlineFlex"),
+
+  // position
+  Absolute: require("./Absolute"),
+  Fixed: require("./Fixed"),
+  Relative: require("./Relative"),
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-module.exports = {
+module.exports = [
+  {
     externals: {
-        "react": "React",
+      "react": "React",
     },
-    entry: "./index.js",
+    entry: "./src/index.js",
     output: {
-        library: "ReactDisposition",
-        libraryTarget: "umd",
-        path: __dirname + "/dist",
-        filename: "ReactDisposition.js"
+      library: "ReactDisposition",
+      libraryTarget: "umd",
+      path: __dirname + "/dist",
+      filename: "ReactDisposition.js"
     },
     module: {
       loaders: [
@@ -22,4 +23,30 @@ module.exports = {
         }
       ]
      }
-}
+  },
+  {
+    externals: {
+      "react": "React",
+    },
+    entry: "./src/index.js",
+    output: {
+      library: "ReactDisposition",
+      libraryTarget: "commonjs2",
+      path: __dirname + "/lib",
+      filename: "index.js"
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: "babel",
+          query: {
+            presets: ["es2015", "react"],
+            plugins: ["transform-object-rest-spread"],
+          }
+        }
+      ]
+     }
+  },
+]
